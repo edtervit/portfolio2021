ee8b301e65b252f5f34e7ab0287887b4c109fb00
Ed <edtervit@hotmail.co.uk>
2024-09-24 13:40:29 +0100

    remove cv from landing

--- components/LandingSection.js
+++ components/LandingSection.js
@@ -1,16 +1,13 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
+import React, {useState, useEffect, useRef, useContext} from "react";
 // import { Canvas } from '@react-three/fiber'
 // import { OrbitControls } from '@react-three/drei'
 import dynamic from "next/dynamic";
 
-const ThreeD = dynamic(() => import("./ThreeD"), { ssr: false });
+const ThreeD = dynamic(() => import("./ThreeD"), {ssr: false});
 
-function LandingSection({ data }) {
-
+function LandingSection({data}) {
   return (
-    <div
-      className="relative flex flex-col items-center justify-center h-screen mx-auto text-center bg-red-700 dark:bg-black dark:text-white"
-    >
+    <div className="relative flex flex-col items-center justify-center h-screen mx-auto text-center bg-red-700 dark:bg-black dark:text-white">
       <div className="absolute top-0 z-0 w-full h-screen">
         <ThreeD />
       </div>
@@ -21,20 +18,24 @@ function LandingSection({ data }) {
         </h1>
 
         {data &&
-          data.button.map((button, index) => (
-            <div
-              key={index}
-              className={`m-3 inline-block animate-bouncy relative animation-delay${index}`}
-            >
-              <a
-                className="px-3 py-1 uppercase transition-all border border-black hover:bg-black dark:border-white hover:text-yellow duration-400 dark:hover:bg-white dark:hover:border-black dark:hover:text-black"
-                href={button.ButtonLink}
-                target={button.newTab ? "_blank" : "_self"}
-              >
-                {button.buttonText}
-              </a>
-            </div>
-          ))}
+          data.button.map((button, index) => {
+            if (index === 0) {
+              return (
+                <div
+                  key={index}
+                  className={`m-3 inline-block animate-bouncy relative animation-delay${index}`}
+                >
+                  <a
+                    className="px-3 py-1 uppercase transition-all border border-black hover:bg-black dark:border-white hover:text-yellow duration-400 dark:hover:bg-white dark:hover:border-black dark:hover:text-black"
+                    href={button.ButtonLink}
+                    target={button.newTab ? "_blank" : "_self"}
+                  >
+                    {button.buttonText}
+                  </a>
+                </div>
+              );
+            }
+          })}
       </div>
       <p className="absolute bottom-10 dark:text-white">scroll down</p>
       <i
